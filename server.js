52b1e9e3c122516ecc0ba1d10d09b6902542d7c8
couldglitch <boosterboo50@gmail.com>
2024-12-27 15:07:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,12 +9,18 @@ const PORT = 3000;
 // Middleware to serve static files
 app.use(express.static('public'));
 
+app.get('/rest/v1/7bg89ds39dbvfd1b5783/g/client/value', (req, res) => {
+    
+    res.sendFile(__dirname + '/public/rest/v1/7bg89ds39dbvfd1b5783/g/client/value.json')
+
+});
+
 // Endpoint to write JSON data to a file via GET request
-app.get('/write-json', (req, res) => {
+app.get('/rest/v1/7bg89ds39dbvfd1b5783/p/client/write', (req, res) => {
     const jsonData = req.query; // Get data from query parameters
 
     // Write the received data to data.json
-    fs.writeFile(path.join(__dirname, 'public/data.json'), JSON.stringify(jsonData, null, 2), (err) => {
+    fs.writeFile(path.join(__dirname, 'public/rest/v1/7bg89ds39dbvfd1b5783/g/client/value.json'), JSON.stringify(jsonData, null, 2), (err) => {
         if (err) {
             return res.status(500).send('Error writing file');
         }
